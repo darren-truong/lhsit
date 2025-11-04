25f37c8947b0e7662366b19e4a043cef25d6646c
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-11-03 19:43:24 -0800

    C6L4

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "drizzle-kit";
+
 import { config } from "./src/config";
 
 export default defineConfig({
-  schema: "src/db",
-  out: "src/db/migrations",
+  schema: "./src/db/schema.ts",
+  out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: config.dbURL,
+    url: config.db.url,
   },
 });
